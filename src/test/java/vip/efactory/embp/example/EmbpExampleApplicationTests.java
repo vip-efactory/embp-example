a12eccc5a7c3a6395662867a7235d037521fe4c5
dbdu <1797890817@qq.com>
2020-08-15 13:59:30 +0800

    添加项目

---
+++ src/test/java/vip/efactory/embp/example/EmbpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.efactory.embp.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
